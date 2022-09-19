e542aac625d3e00af97d8747bf0000aafdb59fe4
Kyle Longakit <kylelongakit@gmail.com>
2022-09-19 16:55:19 +1200

    sprint3done

---
+++ blog/blog.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById('sideNav').style.width ='250px';
+    document.getElementsByClassName('main-content').style.marginLeft="250px";
+}
+
+function closeNav() {
+    document.getElementById('sideNav').style.width ="0";
+    document.getElementsByClassName('main-content').style.marginLeft="0";
+}
